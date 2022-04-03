cc4636b3f3c718fe8875efac41980916505fa518
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-04-03 16:18:33 +0200

    ponemos un limite de intentos y pedimos la direccion de correo electronico

---
+++ clases/ejercicio.py
@@ -0,0 +1,9 @@
+import re
+
+ejecutar=True
+i=5 #utilizaremos la i para definir un numero maximo de intentos para introducir el correo
+
+while ejecutar==True:
+    if i<5:
+        correo=input(str("introduzca su direccion de correo electronico: "))
+    
